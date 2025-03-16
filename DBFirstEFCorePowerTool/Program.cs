e2f8ab2b3bf8f73a017c7d38b318d671f2271203
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-03-17 01:00:54 +0200

    Part 07 Database First [EF Core Power Tool]

---
+++ DBFirstEFCorePowerTool/Program.cs
@@ -0,0 +1,35 @@
+﻿using DBFirstEFCorePowerTool.dbContexts;
+
+namespace DBFirstEFCorePowerTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 07 Database First [EF Core Power Tool]
+
+            ///02. Scaffolding Using Tool[Extension] -> EfCore Power Tool
+            ///  This is Visual Studio extension that simplifies common EF Core tasks like reverse engineering databases,
+            ///  viewing models, and managing migrations. especially useful for Database First workflows ->
+            ///  Generating DbContext and models from an existing database. 
+            ///Note -> After make Scaffolding and try to add like new DB object from DB to Application
+            ///The All files will be regenerated Again.
+            ///if you make configuration in the generated files after make scaffolding, it will lost if you try to make reverse engineering on the DB again.
+            ///If you make configurations in separated file, it will not lost.
+
+            //using NorthwindDbContext dbContext = new NorthwindDbContext();
+            
+            //var employees = dbContext.Employees;
+
+            //if(employees is not null)
+            //{
+            //    foreach (var employee in employees)
+            //    {
+            //        Console.WriteLine(employee.FirstName);
+            //    }
+            //}
+
+            #endregion
+        }
+    }
+}
---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,39 @@
+﻿using DatabaseFirst.dbContexts;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace DatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Part 06 Database First [Command]
+            //It's an approach where you start with a pre-existing database, and EF Core generates the models, DbContext, and relationships.
+            //
+            //Database First Approach Steps : 
+            // 1.Install EF Core Packages [Microsoft.EntityFrameworkCore.SqlServer - Microsoft.EntityFrameworkCore.Tools]
+            // 2.Scaffold the models and DbContext [With "Commands" Or "EfCore Power Tool"]
+
+            //01. Using [Commands] To Scaffold Models Of Remote Database And the DbContext
+            // Scaffold-DbContext -Connection "Server = .; Database = NorthWind; Trusted_Connection = true; TrustServerCertificate = true" -Provider "Microsoft.EntityFrameWorkCore.SqlServer" -Context "MyNorthWindDbContext" -ContextDir "dbContexts" -OutputDir "Models"
+
+            ///Test Retrieve Data.
+            ///MyNorthWindDbContext dbContext = new MyNorthWindDbContext();
+            ///var products = dbContext.Products;
+            ///
+            ///foreach (var product in products)
+            ///{
+            ///    Console.WriteLine(product.ProductName);
+            ///}
+            
+            //02. Using Tool[Extension] -> EfCore Power Tool
+            //  This is Visual Studio extension that simplifies common EF Core tasks like reverse engineering databases,
+            //  viewing models, and managing migrations. especially useful for Database First workflows ->
+            //  Generating DbContext and models from an existing database.
+            
+            #endregion
+        }
+    }
+}
