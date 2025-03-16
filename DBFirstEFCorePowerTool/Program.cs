80a7802e5f0868fdf37aa3de0e0ae7ef25222999
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-03-17 01:30:59 +0200

    Part 08 Run Sql Raw

--- DBFirstEFCorePowerTool/Program.cs
+++ DBFirstEFCorePowerTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DBFirstEFCorePowerTool.dbContexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace DBFirstEFCorePowerTool
 {
@@ -18,7 +19,7 @@ namespace DBFirstEFCorePowerTool
             ///If you make configurations in separated file, it will not lost.
 
             //using NorthwindDbContext dbContext = new NorthwindDbContext();
-            
+
             //var employees = dbContext.Employees;
 
             //if(employees is not null)
@@ -30,6 +31,65 @@ namespace DBFirstEFCorePowerTool
             //}
 
             #endregion
+
+            #region Part 08 Run Sql Raw
+            ///Running/Execute raw SQL query - not use LinQ.
+            ///
+            ///For DQL [Data Query Language]
+            /// 1.FromSqlRaw() - Write Sql Query as string without any Interpolated formatting
+            /// 2.FromSqlInterpolated() - Write Sql Query as Formattable string $"" with Interpolated formatting
+            ///
+            ///For DML [Data Manipulation Language]
+            /// 1.ExecuteSqlRaw()
+            /// 2.ExecuteSqlInterpolated()
+            ///
+            ///Note -> Those "DML" Functions return "int" represent the number of rows affects In DB after execute it.
+
+            #region Example01 - DQL statements like "Select" Using FromSqlRaw().
+
+            //using NorthwindDbContext dbContext = new NorthwindDbContext();
+            //int catId = 1;
+            //var resultRaw = dbContext.Products.FromSqlRaw("Select * From Products where CategoryId = {0}", catId);
+            //foreach (var product in resultRaw)
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+            //Console.WriteLine(); 
+
+            #endregion
+
+            #region Example02 - DQL statements like "Select" Using FromSqlInterpolated().
+
+            //using NorthwindDbContext dbContext = new NorthwindDbContext();
+            //var resultInterpolated = dbContext.Products.FromSqlInterpolated($"Select * From Products where CategoryId = {catId}");
+
+            //foreach (var product in resultInterpolated)
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //} 
+
+            #endregion
+
+            #region Example03 - DML statements like "Update" Using ExecuteSqlRaw()
+            //using NorthwindDbContext dbContext = new NorthwindDbContext();
+
+            //int productId = 1;
+            //var result = dbContext.Database.ExecuteSqlRaw("Update Products Set ProductName = 'Coffee' Where ProductId = {0}",productId);
+            //Console.WriteLine($"Number of rows Affected -> {result}");//Number of rows Affected -> 1
+
+            #endregion
+
+            #region Example04 - DML statements like "Delete" Using ExecuteSqlInterpolated()
+            //using NorthwindDbContext dbContext = new NorthwindDbContext();
+
+            //string productName = "EgyptShai";
+            //var result = dbContext.Database.ExecuteSqlInterpolated($"Delete From Products where ProductName = {productName}");
+            //Console.WriteLine($"Number of rows Affected -> {result}");//Number of rows Affected -> 1
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
